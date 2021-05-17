336f6da591bede53c769d842ec00b8c97137dd77
abychutkin <abychutkin@gmail.com>
2021-05-17 18:07:41 +0300

    Добавлено создание сообщения и отображение всех сообщений

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -1,7 +1,21 @@
 class MessageController < ApplicationController
+  # GET /
   def index
+    @messages = Message.all
   end
 
+  # POST /messages/new
   def new
+    @message = Message.new(message_params)
+    # Задается сообщение об ошибке, если не удалось сохранить сообщение
+    if !@message.save
+      flash[:alert] = "Не удалось добавить сообщение"
+    end
+    redirect_to root_path
+  end
+  
+  private
+  def message_params
+    params.permit(:name, :email, :text)
   end
 end
