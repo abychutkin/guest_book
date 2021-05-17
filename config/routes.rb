ac6dd75732f14b7d743e366c091a2fada09b795e
abychutkin <abychutkin@gmail.com>
2021-05-17 17:45:08 +0300

    Добавлена основа формы и роуты

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'message/index'
-  get 'message/new'
+  root "message#index"
+  #get 'message/index'
+  post 'message/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
